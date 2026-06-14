57fbcfe6cdf9c39c1d9f5296fd53738d97f55ead
loracreativeph <jklorania.ph@gmail.com>
2026-06-14 19:31:37 +0800

    connect render

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,9 @@ const connectDB = require("./connectDB");
 
 const app = express();
 
-// Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+    origin: process.env.FRONTEND_URL,
   })
 );
 
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://bemydate.onrender.com";
 
 // CREATE DATE REQUEST
 export const createDateRequest = (data) => {
