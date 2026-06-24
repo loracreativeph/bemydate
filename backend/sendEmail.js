e282757c80546b791e4433976ea13e77c7c9b386
loracreativeph <jklorania.ph@gmail.com>
2026-06-25 04:21:21 +0800

    Update sendEmail.js

--- backend/sendEmail.js
+++ backend/sendEmail.js
@@ -1,7 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -44,6 +46,7 @@ const sendDateAcceptedEmail = async (
 
   } catch (error) {
     console.error("EMAIL ERROR:", error);
+    throw error;
   }
 };
 
